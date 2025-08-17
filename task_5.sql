7919515bd64d73f846bd98ea875137b8f9d3687e
Yerosen Bekele <yerobeke@gmail.com>
2025-08-17 23:45:02 +0300

    add

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- Select the database
+USE alx_book_store;
+
+-- Insert a single row into the customer table
+INSERT INTO CUSTOMER (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
