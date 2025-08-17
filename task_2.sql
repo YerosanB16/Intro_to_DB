7277589986a703e8d52dcf563c0b72dbec6c8971
Yerosen Bekele <yerobeke@gmail.com>
2025-08-17 21:36:38 +0300

    add

---
+++ task_2.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+CREATE TABLE Books(
+  book_id PRIMARY KEY,
+  title VARCHAR (130),
+  author_id FOREIGN KEY,
+  price DOUBLE,
+  publication_date DATE,
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+CREATE TABLE Authors(
+  author_id PRIMARY KEY,
+  author_name varchar (215)
+);
+CREATE TABLE Customers(
+  customer_id PRIMARY KEY,
+  customer_name VARCHAR(215),
+  email VARCHAR(215),
+  address TEXT
+);
+CREATE TABLE Orders(
+  order_id INT PRIMARY KEY,
+  customer_id INT Foreign Key,
+  order_date DATE,
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+CREATE TABLE Order_Details(
+  orderdetailid PRIMARY KEY,
+  order_id FOREIGN KEY,
+  book_id INT FOREIGN KEY,
+  quantity DOUBLE,
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
+)
